7efb5039e4d6b2da8ca4c74a4292b6b5e02b0d92
Jared <jaredgezahegn@gmail.com>
2025-10-19 17:02:11 +0300

    Ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/https://github.com/JaredGezahegn/movie_react_app.git',  
+  base: '/movie_react_app',  
 })
